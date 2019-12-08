763981644ee771f7bdfe60321025622547653f4b
Jacob Philpott <jacob.gary.philpott@gmail.com>
2019-12-08 16:33:11 -0500

    setting up Flask app skeleton

---
+++ app/root.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask("Jumanji", template_folder="app", static_folder="app")
+
+@app.route("/")
+def root():
+    return render_template("templates/index.html")
+
+if __name__ == "__main__":
+    app.run()
